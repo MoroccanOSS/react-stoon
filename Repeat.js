6ec1fab664630da5de3319039b6f27de16c66d90
Adel Jojo <bigfanjs@gmail.com>
2018-03-19 16:17:46 +0000

    fix: provides the Report component as the default import

--- Repeat.js
+++ Repeat.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Repeat = (props) => {
+export default (props) => {
 	const { $, times, key = indexAsKey, children } = props;
 	return [...Array(times)].map(
 		(el, index) => {
