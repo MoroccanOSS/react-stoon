bf3c7ef60ee8500f5adb99885ea2ba25d7e565c1
Adel Jojo <bigfanjs@gmail.com>
2018-03-19 16:36:50 +0000

    feat: allows users to import multiple exports from the module in one import statement

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import React, {
-	Component,
-	Children,
-} from 'react';
+import LifeCycleAware from "./LifeCycleAware";
+import Repeat from "./Repeat";
+import Show from "./Show";
+import Switch from "./Switch";
+
+export { LifeCycleAware, Repeat, Show, Switch };
