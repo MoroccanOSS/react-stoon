f469be99bb24e43bb7e5d058f54f260db0a11000
Adel Jojo <bigfanjs@gmail.com>
2018-03-19 16:06:59 +0000

    feat: takes LifeCycleAware component into it's own seperate file

---
+++ LifeCycleAware.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class LifeCycleAware extends Component {
+
+	constructor(props) {
+		super(props);
+		const { construct, willMount, didMount } = props;
+		this.construct = construct;
+		this.willMount = willMount;
+		this.didMount = didMount;
+		this.construct && this.construct();
+	}
+
+	componentDidMount() {
+		return this.didMount && this.didMount();
+	}
+
+	componentWillMount() {
+		return this.willMount && this.willMount();
+	}
+	
+	render(props) {
+		const { $, children } = this.props;
+		return (
+			<$ {...this.props}>
+				{children}
+			</$>
+		);
+	}
+}
+
+export default LifeCycleAware;
--- index.js
+++ index.js
@@ -3,35 +3,6 @@ import React, {
 	Children,
 } from 'react';
 
-export class LifeCycleAware extends Component {
-
-	constructor(props) {
-		super(props);
-		const { construct, willMount, didMount } = props;
-		this.construct = construct;
-		this.willMount = willMount;
-		this.didMount = didMount;
-		this.construct && this.construct();
-	}
-
-	componentDidMount() {
-		return this.didMount && this.didMount();
-	}
-
-	componentWillMount() {
-		return this.willMount && this.willMount();
-	}
-	
-	render(props) {
-		const { $, children } = this.props;
-		return (
-			<$ {...this.props}>
-				{children}
-			</$>
-		);
-	}
-}
-
 export const Repeat = (props) => {
 	const { $, times, key = indexAsKey, children } = props;
 	return [...Array(times)].map(
